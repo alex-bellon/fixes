037c31f5567da2fa9b13c6301cc3ff7ad254358a
alex-bellon <alexrbellon@gmail.com>
2019-11-10 15:36:16 -0600

    Initial commit

---
+++ bash.sh
@@ -0,0 +1,14 @@
+sudo !! # re-run previous command with sudo
+
+cd - # change to previous directory
+
+git checkout - # change to previous branch
+
+CTRL+R # search command history
+
+pushd
+popd # navigate through directory stack
+
+!$ # use previous argument
+
+CTRL+L # clear screen
---
+++ hashcat-arch.sh
@@ -0,0 +1,2 @@
+pacman -S hashcat
+yay -S intel-opencl-runtime # need to install to use hashcat without a GPU
