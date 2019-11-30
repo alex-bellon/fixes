b8629dfe390c55a962298753f08ee297d7c661b0
alex-bellon <alexrbellon@gmail.com>
2019-11-29 23:22:29 -0600

    Added pwntools and Java Arch

---
+++ java-arch.sh
@@ -0,0 +1,8 @@
+sudo pacman -S jre-openjdk jdk-open jdk
+source /etc/profile
+
+# archlinux-java status
+# remove any old ones
+# sudo archlinux-java fix
+
+# https://bbs.archlinux.org/viewtopic.php?id=230261
---
+++ pwntools.sh
@@ -0,0 +1,2 @@
+# pip3 install pwntools DOES NOT WORK despite the fact that it "succeeds"
+pip3 install --upgrade git+https://github.com/arthaud/python3-pwntools.git
