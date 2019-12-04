9ba0dae4f9897f266880057002fab0e8bb05156a
alex-bellon <alexrbellon@gmail.com>
2019-12-03 19:34:19 -0600

    Added conversion from GIF to frames

--- bash.sh
+++ bash.sh
@@ -1,17 +1,17 @@
-sudo !! # re-run previous command with sudo
+#####################################################
+# Commands ##########################################
+#####################################################
 
 cd - # change to previous directory
 
-git checkout - # change to previous branch
+echo foo{1,2,3}.txt # prints foo1.txt foo2.txt foo3.txt
 
-CTRL+R # search command history
+git checkout - # change to previous branch
 
 pushd
 popd # navigate through directory stack
 
-!$ # use previous argument
-
-CTRL+L # clear screen
+sudo !! # re-run previous command with sudo
 
 watch 'tail /path/to/log' # monitor changed in output of a command
 
@@ -24,3 +24,13 @@ wget -r -np -nH --cut-dirs=3 -R index.html http://hostname/aaa/bbb/ccc/ddd/
 #  not saving files to hostname folder (-nH),
 #  but to ddd by omitting first 3 folders aaa, bbb, ccc (--cut-dirs=3)
 #  excluding index.html files (-R index.html)
+
+!$ # use previous argument
+
+#####################################################
+# Keyboard Shortcuts ################################
+#####################################################
+
+CTRL+L # clear screen
+
+CTRL+R # search command history
---
+++ gif-to-images.sh
@@ -0,0 +1,1 @@
+convert file.gif frame.png
